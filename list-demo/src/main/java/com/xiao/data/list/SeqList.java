550f9d64eb88a5efbe8297fc8641a3a50f314f13
Jinwei Sun <>
2019-08-28 14:40:54 +0800

    顺序表删除功能

--- list-demo/src/main/java/com/xiao/data/list/SeqList.java
+++ list-demo/src/main/java/com/xiao/data/list/SeqList.java
@@ -79,7 +79,36 @@ public class SeqList {
     }
 
 
+    /**
+     * 顺序表的删除操作，同插入操作相反，删除操作会将后面的元素全部前移
+     * 假如删除 i 位置元素，其实是删除下标为 i-1 的元素，那后面的所有元素都将前移
+     * <p>
+     * 1. 当 i 位置不合法，抛出异常
+     * 2. 取出删除的元素
+     * 3. 从删除元素开始遍历到最后一个元素，将所有元素前移
+     * 4. 表长减1
+     */
+    public int deleteList(SeqList list, int index) {
+        //1. 位置判断
+        if (index < 1 || index > list.length) {
+            System.out.println("position error");
+        }
 
+        //2. 取出待删除元素
+        int deleteNode = list.data[index - 1];
+
+        //3. 元素前移
+        for (int j = index; j < list.length; j++) {
+            list.data[j - 1] = list.data[j];
+        }
+
+        //4. 表长减1
+        list.length--;
+
+        //SeqList(MAX_SIZE=10, data=[20, 30, 30], length=2)] 数组最后一个元素没有被清空
+        System.out.println(Arrays.asList(list));
+        return deleteNode;
+    }
 }
 
 
@@ -91,7 +120,9 @@ class TestSeqList {
         list.setData(new int[]{10, 20, 30});
         list.setLength(3);
 
-        list.insertList(list, 2, 50);
+        //list.insertList(list, 2, 50);
+
+        System.out.println(list.deleteList(list, 1));
     }
 }
 
